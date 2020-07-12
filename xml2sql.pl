5ce6e28d6ffcd187f3043a721dcaf304ebd6312a
Thiago Matos <10cold@gmail.com>
2020-07-12 16:56:40 +0200

    Portuguese translation (#327)
    * Add Portuguese fields to blank template and XML converters.
    
    Note: "pt" is Portuguese (Brazil).
    
    * Run Google Translate to generate Portuguese (Brazil) definitions.
    
    * Part of b words.
    
    * mem-01-b.xml portuguese translation revision.
    
    * mem-02-ch.xml portuguese translation revision.
    
    * mem-03-D.xml portuguese translation revision.
    
    * mem-04-gh.xml portuguese translation revision.
    
    * Update mem-05-H.xml
    
    Portuguese review part 1 of mem-05-H.xml.
    
    * Update mem-05-H.xml
    
    mem-05-H.xml porguese review.
    
    * Update mem-06-j.xml
    
    mem-06-j.xml portugese review. part1
    
    * Update mem-06-j.xml
    
    mem-06-j.xml portuguese revision end.
    
    * Update mem-07-l.xml
    
    mem-07-l.xml portuguese review part 1.
    
    * Update mem-07-l.xml
    
    mem-07-l.xml portuguese review.
    
    * Update mem-08-m.xml
    
    mem-08-m.xml portuguese review.
    
    * mem-08-m.xml and mem-09-n.xml
    
    mem-08-m.xml and mem-09-n.xml portuguese revision.
    
    * Update mem-10-ng.xml
    
    mem-10-ng.xml portuguese review.
    
    * Update mem-11-p.xml
    
    mem-11-p.xml portuguese review.
    
    * Update mem-12-q.xml
    
    mem-12-q.xml portuguese review.
    
    * Update mem-13-Q.xml
    
    mem-13-Q.xml portuguese review.
    
    * Update mem-14-r.xml
    
    mem-14-r.xml
    
    * Update mem-15-S.xml
    
    mem-15-S.xml portuguese review.
    
    * Update mem-16-t.xml
    
    mem-16-t.xml portuguese review.
    
    * Update mem-17-tlh.xml
    
    mem-17-tlh.xml portuguese review.
    
    * Update mem-18-v.xml
    
    mem-18-v.xml portuguese review.
    
    * Update mem-19-w.xml
    
    mem-19-w.xml portuguese review.
    
    * Update mem-20-y.xml
    
    mem-20-y.xml portuguese review
    
    * Update mem-21-a.xml
    
    mem-21-a.xml portuguese review.
    
    * Update mem-22-e.xml
    
    mem-22-e.xml portuguese review.
    
    * Update mem-23-I.xml
    
    mem-23-I.xml portuguese review.
    
    * Update mem-24-o.xml
    
    mem-24-o.xml portuguese translation.
    
    * Update mem-25-u.xml
    
    mem-25-u.xml portuguese review.
    
    * Update mem-26-suffixes.xml
    
    mem-26-suffixes.xml portuguese review.
    
    * Update mem-27-extra.xml
    
    mem-27-extra.xml portuguese review.
    
    Co-authored-by: De'vID <De.vID.jonpIn@gmail.com>

--- xml2sql.pl
+++ xml2sql.pl
@@ -31,13 +31,13 @@ print "PRAGMA foreign_keys=OFF;\n".
       "BEGIN TRANSACTION;\n".
       "CREATE TABLE IF NOT EXISTS \"android_metadata\" (\"locale\" TEXT DEFAULT 'en_US');\n".
       "INSERT INTO android_metadata VALUES('en_US');\n".
-      "CREATE TABLE IF NOT EXISTS \"mem\" (\"_id\" INTEGER PRIMARY KEY ,\"entry_name\" TEXT,\"part_of_speech\" TEXT,\"definition\" TEXT,\"synonyms\" TEXT,\"antonyms\" TEXT,\"see_also\" TEXT,\"notes\" TEXT,\"hidden_notes\" TEXT,\"components\" TEXT,\"examples\" TEXT,\"search_tags\" TEXT,\"source\" TEXT,\"definition_de\" TEXT,\"notes_de\" TEXT,\"examples_de\" TEXT,\"search_tags_de\" TEXT,\"definition_fa\" TEXT,\"notes_fa\" TEXT,\"examples_fa\" TEXT,\"search_tags_fa\" TEXT,\"definition_sv\" TEXT,\"notes_sv\" TEXT,\"examples_sv\" TEXT,\"search_tags_sv\" TEXT,\"definition_ru\" TEXT,\"notes_ru\" TEXT,\"examples_ru\" TEXT,\"search_tags_ru\" TEXT,\"definition_zh_HK\" TEXT,\"notes_zh_HK\" TEXT,\"examples_zh_HK\" TEXT,\"search_tags_zh_HK\" TEXT DEFAULT \"\");\n";
+      "CREATE TABLE IF NOT EXISTS \"mem\" (\"_id\" INTEGER PRIMARY KEY,\"entry_name\" TEXT,\"part_of_speech\" TEXT,\"definition\" TEXT,\"synonyms\" TEXT,\"antonyms\" TEXT,\"see_also\" TEXT,\"notes\" TEXT,\"hidden_notes\" TEXT,\"components\" TEXT,\"examples\" TEXT,\"search_tags\" TEXT,\"source\" TEXT,\"definition_de\" TEXT,\"notes_de\" TEXT,\"examples_de\" TEXT,\"search_tags_de\" TEXT,\"definition_fa\" TEXT,\"notes_fa\" TEXT,\"examples_fa\" TEXT,\"search_tags_fa\" TEXT,\"definition_sv\" TEXT,\"notes_sv\" TEXT,\"examples_sv\" TEXT,\"search_tags_sv\" TEXT,\"definition_ru\" TEXT,\"notes_ru\" TEXT,\"examples_ru\" TEXT,\"search_tags_ru\" TEXT,\"definition_zh_HK\" TEXT,\"notes_zh_HK\" TEXT,\"examples_zh_HK\" TEXT,\"search_tags_zh_HK\" TEXT,\"definition_pt\" TEXT,\"notes_pt\" TEXT,\"examples_pt\" TEXT,\"search_tags_pt\" TEXT DEFAULT \"\");\n";
 
 # Regex to check "en" language fields.
 $valid_en = qr/^[A-Za-z0-9 '":;,.\-?!_\/\()@=%&*{}\[\]<>▶\nàéü+×÷神舟]*$/;
 
 # Language tags.
-@langs = qw(de fa ru sv zh-HK);
+@langs = qw(de fa ru sv zh-HK pt);
 
 # cycle through and print the entries
 foreach $e (@{$data->{database}->{mem}})
@@ -99,7 +99,11 @@ foreach $e (@{$data->{database}->{mem}})
     print $e->{definition_zh_HK}, "','";
     print $e->{notes_zh_HK}, "','";
     print $e->{examples_zh_HK}, "','";
-    print $e->{search_tags_zh_HK}, "');\n";
+    print $e->{search_tags_zh_HK}, "','";
+    print $e->{definition_pt}, "','";
+    print $e->{notes_pt}, "','";
+    print $e->{examples_pt}, "','";
+    print $e->{search_tags_pt}, "');\n";
 }
 
 # print sql file footer
