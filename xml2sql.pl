e6c2d30906a80df3dba46b7ccafdb4cae777f68d
Iikka Hauhio <iikka.hauhio@helsinki.fi>
2020-09-28 17:42:02 +0300

    added finnish to xml2sql.pl

--- xml2sql.pl
+++ xml2sql.pl
@@ -36,7 +36,7 @@ print "PRAGMA foreign_keys=OFF;\n".
       "BEGIN TRANSACTION;\n".
       "CREATE TABLE IF NOT EXISTS \"android_metadata\" (\"locale\" TEXT DEFAULT 'en_US');\n".
       "INSERT INTO android_metadata VALUES('en_US');\n".
-      "CREATE TABLE IF NOT EXISTS \"mem\" (\"_id\" INTEGER PRIMARY KEY,\"entry_name\" TEXT,\"part_of_speech\" TEXT,\"definition\" TEXT,\"synonyms\" TEXT,\"antonyms\" TEXT,\"see_also\" TEXT,\"notes\" TEXT,\"hidden_notes\" TEXT,\"components\" TEXT,\"examples\" TEXT,\"search_tags\" TEXT,\"source\" TEXT,\"definition_de\" TEXT,\"notes_de\" TEXT,\"examples_de\" TEXT,\"search_tags_de\" TEXT,\"definition_fa\" TEXT,\"notes_fa\" TEXT,\"examples_fa\" TEXT,\"search_tags_fa\" TEXT,\"definition_sv\" TEXT,\"notes_sv\" TEXT,\"examples_sv\" TEXT,\"search_tags_sv\" TEXT,\"definition_ru\" TEXT,\"notes_ru\" TEXT,\"examples_ru\" TEXT,\"search_tags_ru\" TEXT,\"definition_zh_HK\" TEXT,\"notes_zh_HK\" TEXT,\"examples_zh_HK\" TEXT,\"search_tags_zh_HK\" TEXT,\"definition_pt\" TEXT,\"notes_pt\" TEXT,\"examples_pt\" TEXT,\"search_tags_pt\" TEXT DEFAULT \"\");\n";
+      "CREATE TABLE IF NOT EXISTS \"mem\" (\"_id\" INTEGER PRIMARY KEY,\"entry_name\" TEXT,\"part_of_speech\" TEXT,\"definition\" TEXT,\"synonyms\" TEXT,\"antonyms\" TEXT,\"see_also\" TEXT,\"notes\" TEXT,\"hidden_notes\" TEXT,\"components\" TEXT,\"examples\" TEXT,\"search_tags\" TEXT,\"source\" TEXT,\"definition_de\" TEXT,\"notes_de\" TEXT,\"examples_de\" TEXT,\"search_tags_de\" TEXT,\"definition_fa\" TEXT,\"notes_fa\" TEXT,\"examples_fa\" TEXT,\"search_tags_fa\" TEXT,\"definition_sv\" TEXT,\"notes_sv\" TEXT,\"examples_sv\" TEXT,\"search_tags_sv\" TEXT,\"definition_ru\" TEXT,\"notes_ru\" TEXT,\"examples_ru\" TEXT,\"search_tags_ru\" TEXT,\"definition_zh_HK\" TEXT,\"notes_zh_HK\" TEXT,\"examples_zh_HK\" TEXT,\"search_tags_zh_HK\" TEXT,\"definition_pt\" TEXT,\"notes_pt\" TEXT,\"examples_pt\" TEXT,\"search_tags_pt\" TEXT DEFAULT \"\",\"definition_fi\" TEXT,\"notes_fi\" TEXT,\"examples_fi\" TEXT,\"search_tags_fi\" TEXT DEFAULT \"\");\n";
 
 # Regex to check "en" language fields.
 $valid_en = qr/^[A-Za-z0-9 '":;,.\-?!_\/\()@=%&*{}\[\]<>▶\nàéü+×÷#神舟]*$/;
@@ -108,7 +108,11 @@ foreach $e (@{$data->{database}->{mem}})
     print $e->{definition_pt}, "','";
     print $e->{notes_pt}, "','";
     print $e->{examples_pt}, "','";
-    print $e->{search_tags_pt}, "');\n";
+    print $e->{search_tags_pt}, "','";
+    print $e->{definition_fi}, "','";
+    print $e->{notes_fi}, "','";
+    print $e->{examples_fi}, "','";
+    print $e->{search_tags_fi}, "');\n";
 }
 
 # print sql file footer
